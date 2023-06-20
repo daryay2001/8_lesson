2987dc600d9c88799eeca3b4a1660e6ebc3c61f6
Daria_Yakymova <yakymovadaria@gmail.com>
2023-06-20 19:17:33 +0300

    Added task 2

--- task_1.py
+++ task_1.py
@@ -10,24 +10,24 @@
 #
 # Написать рекурсивную функцию нахождения степени числа.
 #
-def number_degree(number, degree) -> int:
-    if degree == 1:
-        return number
-    if degree == 0:
-        return 1
-    if degree < 0:
-        raise Exception("Please, enter positive degree")
-    return number * number_degree(number, degree - 1)
-
-try:
-    print(number_degree(number=5, degree=3))
-
-    print(number_degree(number=2, degree=0))
-
-    print(number_degree(number=3, degree=-2))
-
-except Exception as error:
-    print(error)
+# def number_degree(number, degree) -> int:
+#     if degree == 1:
+#         return number
+#     if degree == 0:
+#         return 1
+#     if degree < 0:
+#         raise Exception("Please, enter positive degree")
+#     return number * number_degree(number, degree - 1)
+#
+# try:
+#     print(number_degree(number=5, degree=3))
+#
+#     print(number_degree(number=2, degree=0))
+#
+#     print(number_degree(number=3, degree=-2))
+#
+# except Exception as error:
+#     print(error)
 
 
 #
@@ -36,20 +36,29 @@ except Exception as error:
 # Написать рекурсивную функцию, которая выводит N звезд в ряд, число N задает пользователь.
 # Проиллюстрируйте работу функции примером. (протестировать)
 #
+def my_star(n) -> str:
+    if n > 0:
+        print("*", end="")
+        my_star(n-1)
+    else:
+        return print()
+try:
+    number = int(input("Enter your number: "))
+    my_star(number)
+except Exception as error:
+    print(error)
 
 
-
-
-
-
-
-#
 # Задание 3.
 #
 # Написать рекурсивную функцию, которая вычисляет сумму всех чисел в диапазоне от a до b.
 # Пользователь вводит a и b. Проиллюстрируйте работу функции примером.
 #
 #
+
+
+
+
 ########
 # Дополнительно:
 #
