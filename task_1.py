955f72c75bb3391e46b09d3787148a39593c9bf8
Daria_Yakymova <yakymovadaria@gmail.com>
2023-06-21 14:55:51 +0300

    Added task 4 with problems

--- task_1.py
+++ task_1.py
@@ -88,3 +88,59 @@
 # Напишите рекурсивную функцию, которая принимает одномерный массив из 100 целых чисел
 # заполненных случайным образом и находит позицию, с которой начинается последовательность из 10 чисел,
 # сумма которых минимальна.
+
+
+import random
+
+# V1
+
+# def min_sum_ind(my_array, start = 0):
+#     if start + 10 > len(my_array):
+#         return start
+#     min_sum = sum(my_array[start: start+10]) # Нашла в интернете, что так можно писать для удобства
+#     min_position = start
+#
+#     for i in range(start + 1, len(my_array) - 9): # Чтобы не выйти за пределы массива
+#         actual_sum = sum(my_array[i: i+10])
+#         if actual_sum < min_sum:
+#             min_sum = actual_sum
+#             min_position = i
+#     return min_sum_ind(my_array, start + 1)
+
+# Пишет, что превышена глубина рекурсии
+
+# V2
+def min_sum_index(my_array, start_pos=0):
+    if start_pos + 10 > len(my_array):
+        return start_pos
+
+    sums = []
+    for i in range(start_pos, len(my_array) - 9):
+        actual_sum = sum(my_array[i: i+10])
+        sums.append(actual_sum) # Записываю последовательно суммы в массив 2
+
+    min_sum_index = sums.index(min(sums)) # Вывожу минимальную сумму по индексу
+
+    return min_sum_index(my_array, start_pos + min_sum_index)
+#     Нужно ли тут написать return min_sum_index(my_array, start_pos + 1) ?
+
+try:
+
+    NUMS_SIZE = 100
+    nums = []
+    FIRST = -20
+    LAST = 20
+
+    for i in range(NUMS_SIZE):
+        nums.append(random.randint(FIRST, LAST))
+    # print(min_sum_ind(nums))
+    print(min_sum_index(nums))
+    print(nums[3])
+
+
+except Exception as error:
+    print(error)
+
+
+
+#  Для функции min_sum_index получаю 'int' object is not callable
